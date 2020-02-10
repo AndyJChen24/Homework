3490320958da6669e358d8d6d66c49a4d8ded08c
Andy Chen <ajiajiechen@gmail.com>
2020-02-09 18:14:42 -0800

    Pseudo coded in javascript file for Time Quiz

---
+++ Week4/script.js
@@ -0,0 +1,8 @@
+// create a bar that has let user view highscore and keep track of time
+
+// create highscore page that let user enter their initals and score from quiz
+
+//create 5 multiple choice questions that returns correct or incorrect and keep score 
+//if incorrect timer decrease time
+//if timer hit 0 or all question answered game is over take user to highscore page
+
