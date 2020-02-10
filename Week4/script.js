2d87d5cfe533f3eebfe5a789346ae0b4ebf26878
Andy Chen <ajiajiechen@gmail.com>
2020-02-09 22:06:19 -0800

    Created two div for header bar, one div include a link to high score page(work in progress), other dive is a timer that count down from 60 seconds and updates every second until it hit 0 then an alert will tell user is game over.

--- Week4/script.js
+++ Week4/script.js
@@ -1,5 +1,43 @@
 // create a bar that has let user view highscore and keep track of time
 
+
+// create a short cut to call headerBar Div
+var newDiv = document.getElementById("headerBar")
+//created score tracker 
+var scoreTracker = "View High Scores";
+//create a div that is 50% width with a link to score page and append it to the headerBar div
+var leftDiv = document.createElement("div")
+leftDiv.setAttribute("id", "leftDiv");
+leftDiv.setAttribute("style", "width:50%; display:inline-block");
+leftDiv.innerHTML = scoreTracker.link("#"); 
+newDiv.appendChild(leftDiv);
+
+//create div for timer
+var rightDiv = document.createElement("div");
+rightDiv.setAttribute("id", "rightDiv")
+//create a div that is 50% width with a time tracker and append it to the headerBar div and align the text to the right
+rightDiv.setAttribute("style", "width:50%; display:inline-block; text-align:right");
+newDiv.appendChild(rightDiv);
+// create timer that count down from 60 seconds
+var seconds = 0;
+var interval ;
+// create a timer function that have input of seconds 
+function counter(seconds) {
+   interval = setInterval(function() {
+       // time decrease every seconds
+        seconds--;
+        // write time to rightDiv
+        rightDiv.innerHTML = "Timer " + seconds;
+        // when timer hit 0 game over
+        if(!seconds){
+             clearInterval(interval); 
+             alert("Game Over");
+        }
+   },1000)
+}
+counter(60);
+
+
 // create highscore page that let user enter their initals and score from quiz
 
 //create 5 multiple choice questions that returns correct or incorrect and keep score 
