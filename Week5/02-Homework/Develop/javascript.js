12703a8b117fde4995104427aac228b43eccab73
Andy Chen <ajiajiechen@gmail.com>
2020-02-18 18:12:59 -0800

    added homework 5. reference moments.js. created time blocks in html page.

---
+++ Week5/02-Homework/Develop/javascript.js
@@ -0,0 +1,13 @@
+// get date and year 
+var m = moment();
+// set format for date
+var date = m.format('MMM Do YYYY');
+//set date to header
+$("#currentDay").text(date)
+
+
+
+
+// get button click
+
+    // on button click get text from text box and save it to local storage
