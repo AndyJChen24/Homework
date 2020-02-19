a40e39c47fa97977c1c3b4914b966ac8bbd83d9a
Andy Chen <ajiajiechen@gmail.com>
2020-02-19 01:01:06 -0800

    wrote code to save what user wrote to local storage.

--- Week5/02-Homework/Develop/javascript.js
+++ Week5/02-Homework/Develop/javascript.js
@@ -5,9 +5,25 @@ var date = m.format('MMM Do YYYY');
 //set date to header
 $("#currentDay").text(date)
 
+$(document).ready(function(){
+    
+
+})
 
 
 
 // get button click
+$(".saveBtn").on("click", function(){
+    console.log("is been clicked")
+    console.log(this)
 
-    // on button click get text from text box and save it to local storage
+    // get user input for plans for that time
+    var plans = $(this).prev().val();
+    // point to the sibling element 
+    var keyPointer = $(this).siblings()[0];
+    // set the text inside sibling element as the key to local storage
+    var key = $(keyPointer).text();
+    // saved plans to local storage
+    localStorage.setItem(key, JSON.stringify(plans))
+})
+    
